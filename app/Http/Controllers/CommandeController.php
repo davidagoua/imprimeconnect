5699f91b5b00a134bfd851b06280b3b11e8613bf
Agoua David <cdavidagoua@gmail.com>
2023-12-23 00:27:47 +0000

    desactivate menu stats

--- app/Http/Controllers/CommandeController.php
+++ app/Http/Controllers/CommandeController.php
@@ -27,7 +27,6 @@ class CommandeController extends Controller
 
     public function create(Request $request)
     {
-        activity()->causedBy(auth()->user())->log();
         return $this->edit($request, new Commande());
     }
 
